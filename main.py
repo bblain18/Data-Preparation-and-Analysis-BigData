1ee8c72064d72d148558c7cecf95f5d3dfce9932
Andrew Munoz <amunoz24@nevada.unr.edu>
2019-12-10 18:40:38 -0800

    Created and added new dataset

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ import pandas as pd
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import numpy as np
-import matplotlib.pyplot as plt
 #import tensorflow_datasets.image.septermberSoiltemp.py
 
 _DATA_PATH = "datasets/testDataset.csv"
 _DATASET_PATH = "SeptemberSoilTemperature.csv"
-headers = []
 
 #Data cleaning
 def prep_data():
@@ -19,7 +17,7 @@ def prep_data():
 
     ######### Cleaning Headers ##########
     print(headers)
-
+$
 
     ######### Cleaning Dataset ##########
     #Convert data in column 1 to datetime
@@ -52,75 +50,36 @@ def finalize(aggregate):
     else:
         return (mean, variance)
 
-def findTempSetForDay(targetDay, batch_day, batch_temp):
-    index = np.where(batch_day.numpy() == targetDay)
-    return batch_temp.numpy()[index]
+def findAvgVar(data):
+    aggregate = (0, 0, 0)
+    analysis = (0, 0)
+    for temp in data:
+        aggregate = update(aggregate, temp)
+    analysis = finalize(aggregate)
+    print (analysis)
 
-################# DAILY ANALYSIS ########################################
-def plotDailyAnalysis(days, avgs, min, max, fullMonthInfo, all_val, all_days):
-    fig = plt.figure()
-    analysis = fig.add_subplot(111)
-    # analysis.scatter(all_days, all_val)
-    analysis.scatter(days, avgs, label='Mean temperature')
-    analysis.scatter(days, min, label='Minimum temperature')
-    analysis.scatter(days, max, label='Maximum temperature')
+def find_std():
+    print("std")
 
-    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
-    plt.gcf().text(0.05, 0.15, fullMonthInfo, fontsize=14,
-        verticalalignment='top', bbox=props)
-    plt.subplots_adjust(left=0.3)
-    plt.title('September Soil Temperatures per Day')
-    plt.show()
+def find_mad():
+    print("std")
 
-def dailyAnalysis(batch_day, batch_temp):
-    list_days = []
-    list_avg = []
-    list_min = []
-    list_max = []
+def find_aad():
+    print("std")
 
-    for day in range(1, 31):
-        dailySet = findTempSetForDay(day, batch_day, batch_temp)
-        if dailySet.size > 0:
-            list_avg.append(np.mean(dailySet))
-            list_min.append(np.min(dailySet))
-            list_max.append(np.max(dailySet))
-        else:
-            list_avg.append(0)
-            list_min.append(0)
-            list_max.append(0)
-        list_days.append(day)
-
-    fullMonthInfo = fullMonthAnalysis(batch_temp)
-    plotDailyAnalysis(list_days, list_avg, list_min, list_max, fullMonthInfo, batch_temp, batch_day)
-##################################################################
-
-def fullMonthAnalysis(batch_temp):
-    # Analysis
-    mean = np.mean(batch_temp.numpy())
-    variance = np.var(batch_temp.numpy())
-    std = np.std(batch_temp.numpy())
-
-    return '\n'.join((
-        r'Mean=%.4f' % (mean, ),
-        r'Variance=%.4f' % (variance, ),
-        r'Standard Deviation=%.4f' % (std, )))
+def find_distribution():
+    print("std")
 
 def main():
-    tf.enable_eager_execution()
-    #Load data
     sst_train, info = tfds.load("september_soil_temp", split="train", with_info=True)
-    #Shuffle batch
-    sst_train_batch = sst_train.shuffle(20).padded_batch(240, padded_shapes = {"day": [], "temperature": []})
-    # Assert instance
-    assert isinstance(sst_train_batch, tf.data.Dataset)
-    # Numpy matrix vars
-    temperature = []
-    day = []
-    # Take matrix from batch
-    for sst_example in sst_train_batch:
-        day, temperature = sst_example["day"], sst_example["temperature"]
 
-    dailyAnalysis(day, temperature)
+    print(info)
+
+    data = prep_data()
+
+    for sst_example in sst_train.take(1):  # Only take a single example
+        image, label = sst_example["day"], sst_example["temperature"]
+        print(image)
 
 
 main()
